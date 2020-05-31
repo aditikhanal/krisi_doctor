2f5e0aaef05dc41f0c43f2bcffb1c6cc5a58c78a
Kalpana-Joshi <kalpanajoshi1059@gmail.com>
2020-05-31 20:38:13 +0545

    First commit

--- src/pages/Screen4.js
+++ src/pages/Screen4.js
@@ -1,9 +1,18 @@
 //This is an example code for NavigationDrawer//
 import React, { Component } from 'react';
 //import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Linking} from 'react-native';
 // import all basic components
+import {  Card} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const openDial=()=>{
+  if(Platform.OS === "android"){
+      Linking.openURL("tel:9848983054")
+   }else{
+      Linking.openURL("telprompt:9848983054")
+   }    
+}
 export default class Screen4 extends Component {
   //Screen4 Component
   render() {
@@ -13,9 +22,26 @@ export default class Screen4 extends Component {
           <Text style={styles.text2}>Plant disease is one of the major problems in agriculture which reduces production rate and increases financial costs only. So, Plant diseases detection is one of the important for agricultural applications to increase the yield. </Text>
           <Text style={styles.text3}>              Our System is built using the inception model containing CNN based image classification system. The disease can be classified according to the patterns seen in the diseased leaves of the plant. Disease detection involves the steps like image acquisition, image pre-processing, feature extraction and classification. Also information about the classified disease is provided.</Text>
 
-          <Text style={styles.text1}>For further details call us at:</Text>
-          <Text style={styles.text}>9876543210</Text>
-          <Text style={styles.text}>Email: krisidoctor123@gmail.com</Text>
+          <Text style={styles.text1}>For further details:</Text>
+
+          <View style={styles.card}> 
+              <Card style={styles.mycard} onPress={()=>{
+                    Linking.openURL("mailto:abc@abc.com")
+                }}>
+                    <View style={styles.cardContent }>
+                    <Icon name="envelope" size={30} color="#006aff" />
+                    <Text style={styles.mytext} >krisidoctor@gmail.com</Text>
+                    </View>
+                </Card>
+
+                <Card style={styles.mycard} onPress={()=>openDial()}>
+                    <View style={styles.cardContent }>
+                    <Icon name="phone" size={30} color="#006aff" />
+                    <Text style={styles.mytext} >9876543210</Text>
+                    </View>
+                </Card>
+              </View>
+
     </View>  
     );
   }
@@ -24,10 +50,15 @@ export default class Screen4 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 30,
+    padding:20,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#70B086'
+  },
+  card: {
+    flex: 1,
+    backgroundColor: '#70B086',
+    paddingLeft: 10,
   },
   text4: {
     fontSize:25,
@@ -40,16 +71,16 @@ const styles = StyleSheet.create({
     fontStyle:"italic"
   },
   text2: {
-    fontSize:15,
+    fontSize:17,
     color: "black",
-    margin:8,
+    margin:7,
     alignItems: "center", 
     justifyContent: "center"
   },
   text3: {
-    fontSize:15,
+    fontSize:17,
     color: "black",
-    margin: 8,
+    margin: 7,
     alignItems: "center", 
   },
   text1: {
@@ -57,7 +88,8 @@ const styles = StyleSheet.create({
     color: "black",
     margin:8,
     alignItems: "center", 
-    justifyContent: "center"
+    justifyContent: "center",
+    textDecorationLine: 'underline',
   },
   text: {
     fontSize:19,
@@ -67,4 +99,19 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     justifyContent: "center"
   },
+  mycard:{
+    margin:5,
+    backgroundColor:"#70B086",
+    flexDirection:"row"
+  },
+    cardContent:{
+    padding:4,
+    flexDirection:"row"
+  },
+  mytext:{
+    fontSize:17,
+    marginTop:1,
+    marginLeft:20,
+    color:"black"
+},
 });
